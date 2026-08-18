c316afb7b53aa1f4858822ed128be97302c45afb
itzikst <itzik.stauber@gmail.com>
2026-08-18 17:59:06 +0300

    Align map bottom overlays and shorten satellite view attribution

--- js/map-manager.js
+++ js/map-manager.js
@@ -147,7 +147,7 @@ export function initMap(domId, esriApiKey) {
 
     // 2. Satellite View Layer (Esri World Imagery + Imagery Reference Labels)
     const satImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        attribution: 'Tiles &copy; Esri',
         maxZoom: 19
     });
 
